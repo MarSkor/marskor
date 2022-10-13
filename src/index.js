d3e6e0efb76066e7a7374e97e5c167bbf4cc062c
MarSkor <maskor_@outlook.com>
2022-10-13 20:18:07 +0200

    second commit, fixes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import "./styles/main.scss";
 import "./components/nav";
 import "./components/theme-switcher";
+import AOS from 'aos';
+AOS.init({delay: 1,});
 
 const feather = require('feather-icons');
 feather.replace()
+
