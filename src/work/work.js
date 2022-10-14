c930e265f449f5257e194f681cb01e63477be188
MarSkor <maskor_@outlook.com>
2022-10-14 13:31:57 +0200

    style fix

--- src/work/work.js
+++ src/work/work.js
@@ -1,6 +1,8 @@
 import "../styles/main.scss";
 import "../components/theme-switcher";
-
+import "../components/nav";
+import AOS from 'aos';
+AOS.init({delay: 1,});
 
 const feather = require('feather-icons');
 feather.replace()
